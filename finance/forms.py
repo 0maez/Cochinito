cc905125b530cd31a063a5d2b75c02e93121751d
Stefano Boada <stefanox88@gmail.com>
2025-03-03 21:56:25 -0400

    Vamos a ver si no la cagox2

--- finance/forms.py
+++ finance/forms.py
@@ -7,4 +7,10 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2"]
+        fields = ["first_name", "last_name", "email", "password"]
+        
+
+class ProfileForm(forms.ModelForm):
+    class Meta: 
+        model = User
+        fields = ["username", "email", "first_name", "last_name"]
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
+from .forms import ProfileForm
 
 def register(request):
     if request.method == "POST":
@@ -9,4 +11,15 @@ def register(request):
             return redirect("login")  # Redirige al login después del registro
     else:
         form = RegisterForm()
-    return render(request, "accounts/register.html", {"form": form})
+    return render(request, "finance/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, "finance/profile.html", {"form": form})
